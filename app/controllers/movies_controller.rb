f5f34e73697ab436431fa2f28d30b88d80cc1dd5
eddie <eddie@visionbundles.com>
2014-11-08 22:34:54 +0800

    Add js view and refactor movie partial

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,7 +2,10 @@ class MoviesController < ApplicationController
 
   # GET /movies
   def index
-    @movies = Movie.where(published: true)
+    respond_to do |format|
+      format.html
+      format.js { @movies = Movie.where(published: true).page(params[:page]) }
+    end
   end
 
   # GET /movies/1
